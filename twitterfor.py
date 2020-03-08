31c6f7f5bf6071bc0d4c04eefbb38d1341caadf0
X1nto <burtkni@yandex.ru>
2020-03-08 14:27:27 +0400

    added some functions from API

--- twitterfor.py
+++ twitterfor.py
@@ -2,6 +2,14 @@
 import tweepy, os, sys, time
 from pathlib import Path
 
+logo = """
+████████╗ ██╗       ██╗██╗████████╗████████╗███████╗██████╗   ███████╗ █████╗ ██████╗            
+╚══██╔══╝ ██║  ██╗  ██║██║╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗  ██╔════╝██╔══██╗██╔══██╗           
+   ██║    ╚██╗████╗██╔╝██║   ██║      ██║   █████╗  ██████╔╝  █████╗  ██║  ██║██████╔╝           
+   ██║     ████╔═████║ ██║   ██║      ██║   ██╔══╝  ██╔══██╗  ██╔══╝  ██║  ██║██╔══██╗           
+   ██║     ╚██╔╝ ╚██╔╝ ██║   ██║      ██║   ███████╗██║  ██║  ██║     ╚█████╔╝██║  ██║  ██╗██╗██╗
+   ╚═╝      ╚═╝   ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝  ╚═╝      ╚════╝ ╚═╝  ╚═╝  ╚═╝╚═╝╚═╝
+"""
 
 print ("Checking config file...")
 config = Path("./tokenconfig.py")
@@ -16,34 +24,43 @@ if config.is_file():
     print ("Found config!")
     print ("Initialising...")
     time.sleep(1)
-    print ("\nTwitter For [redacted]")
-    print ("Is this tweet a reply? Use Y or N")
-
-
+    print (logo)
 
     while True:
-        isitareply = input("[Y/N]:")
-        if isitareply in ['Y', 'N']:
+        choose = input("1)Tweet\n2)Upload media\n3)Reply to a Tweet\n4)Retweet\n\n[Your Choice]:")
+        if choose in ['1', '2', '3', '4']:
             break
 
-    if isitareply == 'Y':
-        tweet = input("Make a reply: ")
-
-        inreplyto = input("What status is this replying to? (Use an ID of tweet): ")
-
-        api.update_status(status =(tweet), in_reply_to_status_id =(inreplyto))
-        print ("Succesfully replied!")
-    else:
+    if choose == '1':
         tweet = input("Make a tweet: ")
-
+            
         api.update_status(status =(tweet))
         print ("Succesfully tweeted!")
+    elif choose == '2':        
+        media = input("Please specify a file: ")
+        extensions = ['.png', '.jpg', '.gif', '.mp4', '.mov']
         
+        if media.endswith(('.png', '.jpg', '.gif', '.mp4', '.mov')):
+            api.media_upload(filename =(media))
+            print ("Succesfully Tweeted!")
+        else:
+            print ("Provide a valid media format (see README)")
+            
+    elif choose == '3':
+        tweet = input("Make a reply: ")
+        inreplyto = input("What status is this replying to? (Use an ID of tweet): ")
+        
+        api.update_status(status =(tweet), in_reply_to_status_id =(inreplyto))
+        print ("Succesfully replied!")
+        
+    elif choose == '4':
+        retweet = input("Provide an ID of tweet that you wish to retweet: ")
+        api.retweet(id =(retweet))
+        print ("Succesfully retweeted!")
 else:
     print ("Config not found! Creating config from template...")
     os.rename(r'./tokenconfig.py.template',r'./tokenconfig.py')
     print ("restarting...")
     time.sleep(1.2)
     os.execl(sys.executable, sys.executable, *sys.argv)
-
     
