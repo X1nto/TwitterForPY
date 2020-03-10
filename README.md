29cc72e9d4a2187c1f9ffae1c1ad1b474ad34346
X1nto <burtkni@yandex.ru>
2020-03-10 16:50:35 +0400

    Updated script

--- README.md
+++ README.md
@@ -7,14 +7,17 @@ First you need to create [Developer Account](https://developer.twitter.com) on T
 After sign up, create the new Application
 ##### Note: Your application name will be important, as it will be replacing "Twitter for (Android/iPhone/Web Client)" fields. If you want a custom "Twitter For" field, name it "Twitter for <your choice>". If you change name later, this field will still use old name. Be careful when choosing a name  
 After creating an application, go to Tokens tab and copy every token from there, now clone this repo and follow next steps:  
-  
-Open `tokenconfig.py.template` and fill out all the fields with your application tokens (do not rename this file!)  
-Now open terminal and install tweepy module: `pip install tweepy`  
+
+open terminal and install tweepy module: `pip install tweepy`  
 Run the program via `twitterfor.py`  
+Program will ask you to provide tokens, do what it says__
+After you provided tokens, it will self restart__
 
 ![Screenshot](https://github.com/X1nto/twitter-for-x/blob/master/Screenshot.png)  
 Now you'll see this menu, choose any option you want and fill out everything  
-
-##### Second option isn't working as of now but it will be fixed soon  
-
 After you fill out everything there, congrats, you have succesfully tweeted via Gucci Smart Toilet.  
+
+## Notes:
+Second option isn't working as of now but it will be fixed soon  
+Listing and sending DMs require special permissions. On app console, go to permissions tab, click edit and select 3rd option and click save. for this change to take effect you need to regenerate all tokens. If you're already logged in to this app, you need to edit config.py and overwrite tokes there.  
+Listing DMs will output raw JSON strings and I have no idea how to fix it, if you know anything Pull Request would be highly appreciated.
--- twitterfor.py
+++ twitterfor.py
@@ -12,68 +12,193 @@ logo = """
 """
 
 print ("Checking config file...")
-config = Path("./tokenconfig.py")
+config = Path("./config.py")
 if config.is_file():
-    import tokenconfig as cfg
+    import config as cfg
     
-    auth = tweepy.OAuthHandler(cfg.ConsumerAPI['api_key'], cfg.ConsumerAPI['api_secret_key'])
-    auth.set_access_token(cfg.AccessToken['access_token'], cfg.AccessToken['access_token_secret'])
+    auth = tweepy.OAuthHandler(cfg.api_key, cfg.api_secret_key)
+    auth.set_access_token(cfg.AccessToken, cfg.AccessSecretToken)
 
     api = tweepy.API(auth)
 
     print ("Found config!")
     print ("Initialising...")
-    time.sleep(1)
-    print (logo)
-
-    while True:
-        choose = input("1)Fetch your timeline\n2)Tweet\n3)Upload media (Not Working)\n4)Reply to a Tweet\n5)Retweet\n6)List DM\n7)Send DM\n\nq)Quit\n\n[Your Choice]:")
-        if choose in ['1', '2', '3', '4', '5', '6', '7', 'q']:
-            break
-        
-    if choose == '1':
-        s = ""
-        tweets = api.home_timeline(count = '20')
-        for status in tweets:
-            print ('-----------------\n@' + status.user.name + ' (' + str(status.user.id) + ')' + "\n-----------------\n" + status.text, '\n\n' + 'Tweet ID(' + str(status.id) + ')')
-        
-    elif choose == '2':
-        tweet = input("Make a tweet: ")
-        api.update_status(status =(tweet))
-        print ("Succesfully tweeted!")
-        
-    elif choose == '3':        
-        media = input("Please specify a file: ")
-        api.media_upload(filename =(media))
-        print ("Succesfully Tweeted!")
-
-    elif choose == '4':
-        tweet = input("Make a reply: ")
-        inreplyto = input("What status is this replying to? (Use an ID of tweet): ")        
-        api.update_status(status =(tweet), in_reply_to_status_id =(inreplyto))
-        print ("Succesfully replied!")
-        
-    elif choose == '5':
-        retweet = input("Provide an ID of tweet that you wish to retweet: ")
-        api.retweet(id =(retweet))
-        print ("Succesfully retweeted!")
     
-    elif choose == '6':
-        dmcnt = input("How many DMs do you want to list: ")
-        dmlist = api.list_direct_messages(count =(dmcnt))
-        print (dmlist)
-    
-    elif choose == '6':
-        dmid = input("Direct ID: ")
-        dmtext = input("Text: ")
-        api.send_direct_message(recipient_id =(dmid), text =(dmtext))
-        
-    elif choose == 'q':
-        print ("Goodbye!")
-        
+    def mainmenu():
+        time.sleep(1)
+        print (logo)
+        user = api.me()
+        print ("Logged in as: @" + user.screen_name)
+        while True:
+            choose = input("\n1)Fetch your timeline\n2)Tweet\n3)Upload media (Not Working)\n4)Reply to a Tweet\n5)Retweet\n6)List DM\n7)Send DM\n\nq)Quit\n\n[Your Choice]:")
+            if choose in ['1', '2', '3', '4', '5', '6', '7', 'q']:
+                break
+            
+        if choose == '1':
+            def timeline():
+                tweets = api.home_timeline(count = '20')
+                for status in tweets:
+                    print ('-----------------\n@' + status.user.name + ' (' + str(status.user.id) + ')' + "\n-----------------\n" + status.text, '\n\n' + 'Tweet ID(' + str(status.id) + ')')
+                while True:
+                    choose1 = input("\n1)Re-fetch timeline\n2)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                    if choose1 in ['1', '2', 'q']:
+                        break
+                if choose1 == '1':
+                    timeline()
+                elif choose1 == '2':
+                    mainmenu()
+                elif choose1== 'q':
+                    print ("Goodbye!")
+                    
+            timeline()
+            
+        elif choose == '2':
+            def tweet():
+                tweet = input("Make a tweet: ")
+                api.update_status(status =(tweet))
+                print ("Succesfully tweeted!")
+                
+                while True:
+                    choose2 = input("\n1)Make another tweet\n2)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                    if choose1 in ['1', '2', 'q']:
+                        break
+                if choose2 == '1':
+                    tweet()
+                elif choose2 == '2':
+                    mainmenu()
+                elif choose2 == 'q':
+                    print ("Goodbye!")
+                    
+            tweet()
+            
+        elif choose == '3':
+            def mediupload():
+                media = input("Please specify a file: ")
+                api.media_upload(filename =(media))
+                print ("Succesfully Tweeted!")
+                
+                while True:
+                    choose3 = input("\n1)Upload another media\n2)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                    if choose1 in ['1', '2', 'q']:
+                        break
+                if choose3 == '1':
+                    mediupload()
+                elif choose3 == '2':
+                    mainmenu()
+                elif choose3 == 'q':
+                    print ("Goodbye!")
+                    
+            mediupload()
+
+        elif choose == '4':
+            def reply():
+                inreplyto = input("What status is this replying to? (Use an ID of tweet): ")
+                replytxt = input("Make a reply: ")
+                api.update_status(status =(replytxt), in_reply_to_status_id =(inreplyto))
+                print ("Succesfully replied!")
+                while True:
+                    choose4 = input("\n1)Reply to another post\n2)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                    if choose4 in ['1', '2', 'q']:
+                        break
+                if choose4 == '1':
+                    reply()
+                elif choose4 == '2':
+                    mainmenu()
+                elif choose4 == 'q':
+                    print ("Goodbye!")
+                    
+            reply()
+            
+        elif choose == '5':
+            retweetid = input("Provide an ID of tweet that you wish to retweet: ")
+            def retweet():
+                api.retweet(id =(retweetid))
+                print ("Succesfully retweeted!")
+                
+                while True:
+                    choose5 = input("\n1)Retweet another tweet\n2)Unretweet this tweet\n3)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                    if choose5 in ['1', '2', '3', 'q']:
+                        break
+                if choose5 == '1':
+                    retweet()
+                elif choose5 == '2':
+                    api.unretweet(id =(retweetid))
+                    print ("Succesfully unretweeted!")
+                    mainmenu()
+                elif choose5 == '3':
+                    mainmenu()
+                elif choose5 == 'q':
+                    print ("Goodbye!")
+                    
+            retweet()
+            
+        elif choose == '6':
+            def dmlist():
+                dmlist = api.list_direct_messages()
+                print (dmlist)
+                
+                while True:
+                    choose6 = input("\n1)Refresh\n2)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                    if choose1 in ['1', '2', 'q']:
+                        break
+                if choose6 == '1':
+                    dmlist()
+                elif choose6 == '2':
+                    mainmenu()
+                elif choose6 == 'q':
+                    print ("Goodbye!")
+                    
+            dmlist()
+        elif choose == '7':
+            def senddm():
+                dmid = input("Direct ID: ")
+                def send():
+                    dmtext = input("Text: ")
+                    api.send_direct_message(recipient_id =(dmid), text =(dmtext))
+                    while True:
+                        choose7 = input("\n1)Send another DM to this person\n2)Refresh this DM\n3)Return to main menu\n\nq)Quit\n\n[Your Choice]:")
+                        if choose7 in ['1', '2', '3', 'q']:
+                            break
+                    if choose7 == '1':
+                        send()
+                    elif choose7 == '2':
+                        def refresh():
+                            refresh = api.get_direct_message(id =(dmid))
+                            print (refresh)
+                            
+                            while True:
+                                choose8 = input("\n1)refresh this Dm\n\nq)Quit\n\n[Your Choice]:")
+                                if choose1 in ['1', 'q']:
+                                    break
+                            if choose8 == '1':
+                                refresh()
+                            elif choose8 == 'q':
+                                print ("Goodbye!")
+                                
+                        tweet()
+                        refresh()
+                    elif choose7 == '3':
+                        senddm()
+                    elif choose7 == '4':
+                        mainmenu()
+                    elif choose7 == 'q':
+                        print ("Goodbye!")
+                send()
+            senddm()
+            
+        elif choose == 'q':
+            print ("Goodbye!")
+            
+    mainmenu()
 else:
-    print ("Config not found! Creating config from template...")
-    os.rename(r'./tokenconfig.py.template',r'./tokenconfig.py')
-    print ("restarting...")
-    time.sleep(1.2)
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    print ("Config not found! Creating config...")
+    with open('config.py', 'a') as file:
+        apikey = input("Please enter your api key: ")
+        file.write("api_key: " + "'" + apikey + "'")
+        apiseckey = input("Please enter your API Secret Key: ")
+        file.write("\napi_secret_key: " + "'" + apiseckey + "'")
+        acctok = input("Please enter your access token: ")
+        file.write("\nAccessToken: " + "'" + acctok + "'")
+        accsectok = input("Please enter your Access Secret Token: ")
+        file.write("\nAccessSecretToken: " + "'" + accsectok + "'")
+        
